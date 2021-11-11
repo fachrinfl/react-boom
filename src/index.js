731aec0d5ec636b6ddee2af55923f020058fffb2
Fachri Naufal <fachrinfl@gmail.com>
2021-11-11 17:33:37 +0700

    remove unused file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Starting Project</h1>
+    </div>
   </React.StrictMode>,
   document.getElementById('root'),
 );
